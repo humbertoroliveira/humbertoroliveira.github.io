f614dcd5d8c32afc42695c451d794b5769b2fd82
Humberto de Oliveira <55997445+humbertoroliveira@users.noreply.github.com>
2020-03-06 08:32:21 +0000

    Updates

---
+++ precache-manifest.ee19c353e4f864e2e7d620a7ee8cf489.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25718e976d850f6d6c0498fa5703013",
+    "url": "/csv-to-segment/index.html"
+  },
+  {
+    "revision": "4811b45d791ef3945856",
+    "url": "/csv-to-segment/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ff686add86f89e31869",
+    "url": "/csv-to-segment/static/js/2.353e5764.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/csv-to-segment/static/js/2.353e5764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4811b45d791ef3945856",
+    "url": "/csv-to-segment/static/js/main.d932a621.chunk.js"
+  },
+  {
+    "revision": "6ec20b36c20bb2566add",
+    "url": "/csv-to-segment/static/js/runtime-main.2ef6ec73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/csv-to-segment/static/media/logo.5d5d9eef.svg"
+  }
+]);
